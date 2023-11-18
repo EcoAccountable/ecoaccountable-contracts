fc4a19f363e43e517f9681855b2e703cd55d4a8e
Ai Suzuki <asuzuki@blockchains.com>
2023-11-18 18:50:19 +0100

    add deployed contract addresses

--- packages/hardhat/README.md
+++ packages/hardhat/README.md
@@ -35,3 +35,9 @@ sequenceDiagram
     Protocol ->> WorldBoatClimateActions: fulfill matching order if project Id found 
 
 ```
+
+contracts in arbitrumSepolia:
+deployer = 0x78BA6B002adEf3973622b1632e4537C39c6c1797
+deploying "WorldBoatProtocol" (tx: 0xd128b04bed0387c622bbeb83ba8a7feb592cad88a78d4d467dd65ef4ac36bd48)...: deployed at 0x2753D07A5B040DF342721e33058942fAd9Bb3E3e with 806763 gas
+deploying "WorldBoatClimateActions" (tx: 0xbb82df44f018a30e89209367327a63557299991ac589a68371e9294e75e0a0b9)...: deployed at 0xfdd6076e296eF65E211A4c894FEfC52880e86935 with 2192662 gas
+deploying "WBToken" (tx: 0xc7513580820fc14065e550758301c6ca475575ac3ac73fa99f4de02b5ae0f58a)...: deployed at 0x85B815f9B358d90C4338C216B9F346BE47F820fA with 650902 gas
--- packages/hardhat/deploy/00_deploy_wbca_contracts.ts
+++ packages/hardhat/deploy/00_deploy_wbca_contracts.ts
@@ -18,20 +18,33 @@ const deployWorldBoatClimateActionsContract: DeployFunction = async function (hr
     with a random private key in the .env file (then used on hardhat.config.ts)
     You can run the `yarn account` command to check your balance in every network.
   */
-  const { deployer, treasury } = await hre.getNamedAccounts();
+  const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
   console.log("deployer================" + deployer);
 
-  await deploy("WorldBoatClimateActionsContract", {
+  const protocol = await deploy("WorldBoatProtocol", {
     from: deployer,
     // Contract constructor arguments
-    args: [treasury],
+    args: [],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
   });
+
+  const wbca = await deploy("WorldBoatClimateActions", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [protocol.address],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  const p = await hre.ethers.getContract("WorldBoatProtocol", deployer);
+  await p.setup(wbca.address);
 
   // Get the deployed contract
   // const yourContract = await hre.ethers.getContract("YourContract", deployer);
@@ -41,4 +54,4 @@ export default deployWorldBoatClimateActionsContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployWorldBoatClimateActionsContract.tags = ["WorldBoatClimateActionsContract"];
+deployWorldBoatClimateActionsContract.tags = ["WorldBoatClimateActions"];
